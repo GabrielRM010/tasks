87283617f64c5649948c0f0ef199948d584e9f9c
GABRIELRM010 <gabriel.r.maringolo@gmail.com>
2024-09-24 08:45:01 -0300

    adicionando botao de tarefas

--- src/screens/Add_Task.js
+++ src/screens/Add_Task.js
@@ -7,30 +7,43 @@ import { Text,
     TouchableWithoutFeedback,
      Modal 
     } from "react-native";
+    import  Icon  from "react-native-vector-icons/FontAwesome6";
+    import moment from "moment";
+    import DateTimePicker from "@react-native-community/datetimepicker"
 
     export default class AddTask extends Component{
         render(){
             return(
                 <Modal
                  transparent={true}
-                    visible={true}
+                    visible={this.props.visivel}
+                    onRequestClose={this.props.cancelar}
                     animationType="slide"
                     >
-                   <TouchableWithoutFeedback>
+                   <TouchableWithoutFeedback
+                   onPress={this.props.cancelar}>
                     <View style={styles.fundo}></View>
                    </TouchableWithoutFeedback>
                    <View style={styles.principal}>
                     <Text style={styles.cabecalho}>Nova Tarefa</Text>
+                    <View style="styles.container">
+                    <Icon name="clipboard-check" size={25}></Icon>
                     <TextInput
+                         style={styles.input}
                         placeholder="Descrição da Tarefa"
-                        style={styles.input}
                     />
-                    <View>
-                        <TouchableOpacity>
-                            <Text>Cancelar</Text>
+                    </View>
+                    <TouchableOpacity  style="styles.container">
+                        <Icon name="calendar" size={25}></Icon>
+                        <Text style={styles.data}>data atual</Text>
+                    </TouchableOpacity>
+                    <View style={styles.botoes}>
+                        <TouchableOpacity
+                        onPress={this.props.cancelar}>
+                            <Text  style={styles.botao}>Cancelar</Text>
                         </TouchableOpacity>
                         <TouchableOpacity>
-                            <Text>Salvar</Text>
+                            <Text  style={styles.botao}>Salvar</Text>
                         </TouchableOpacity>
                     </View>
                    </View>
@@ -51,11 +64,36 @@ import { Text,
             flex: 1,
             backgroundColor: '#FFF'
         },
-        cabecalho{
+        cabecalho:{
             backgroundColor: '#B13B44',
             color: '#FFF',
             fontSize: 20,
             TextAlling: 'center',
-            padding: 15
+            padding: 25
+        },
+        input:{
+            width: '85%',
+            margin: 15,
+            borderWidth: 1,
+            borderColor:'E3E3E3',
+            borderRadius: 6
+        },
+        container:{
+            flexDirection:'row',
+            alignItems: 'center',
+            marginLeft: 15,
+            paddingVertical: 10
+        },
+        botoes:{
+            flexDirection:'row',
+            justifyContent: 'flex-end'
+        },
+        botao:{
+            margin: 20,
+            marginRight: 30,
+            color: 'B13B44'
+        },
+        data:{
+            margin: 10
         }
     })
